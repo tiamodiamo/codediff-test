d84f13e41dc87498c900939ab063acafe4ea42e8
jayKing <8114173+k-tiamo@user.noreply.gitee.com>
2024-04-07 13:40:15 +0800

    main

--- src/main/java/com/jacotest/jacocotest/user/action/LoginAction.java
+++ src/main/java/com/jacotest/jacocotest/user/action/LoginAction.java
@@ -17,12 +17,42 @@ public class LoginAction {
         String password = userLoginParam.getPassword();
         if (username.equals("admin")&&password.equals("123456")){
             log.info("用户登录成功");
+            int sum = 0;
+            for (int i = 0; i < 100; i++) {
+                sum +=i;
+            }
+            log.info("success:sum = " + sum);
             return "登录成功";
         }else {
+
             log.info("用户登录失败");
             return "登录失败";
         }
 
+    }
+
+    public String doSomething(){
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum +=i;
+        }
+        log.info("err:sum = " + sum);
+
+        for (int i = 0; i < 100; i++) {
+            sum +=i;
+        }
+        log.info("err:sum = " + sum);
+
+        for (int i = 0; i < 100; i++) {
+            sum +=i;
+        }
+        log.info("err:sum = " + sum);
+
+        for (int i = 0; i < 100; i++) {
+            sum +=i;
+        }
+        log.info("err:sum = " + sum);
+        return "";
     }
 
 
